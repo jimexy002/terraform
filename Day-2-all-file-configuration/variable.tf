3036829b148e2ce07db00c0d7fbe2ac7cc4131ed
Deepak <dsahu353490@gmail.com>
2026-06-02 16:49:16 +0530

    day1/-2

---
+++ Day-2-all-file-configuration/variable.tf
@@ -0,0 +1,12 @@
+variable "ami_id" {
+    description = "value"
+    default = "ami-00e801948462f718a"
+
+
+}
+variable "instance_type" {
+    default = "t2.micro"
+}
+variable "babu" {
+    default = "my-ec2"
+}
