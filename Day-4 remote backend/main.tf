904c2c7bdce06e02d74e54a5ff09fb4d9eaa7f89
Deepak <dsahu353490@gmail.com>
2026-06-04 14:27:03 +0530

    updated1 backend

--- Day-4 remote backend/main.tf
+++ Day-4 remote backend/main.tf
@@ -1,7 +1,18 @@
 resource "aws_instance" "ok" {
-    ami =      "ami-00e801948462f718a"
-instance_type = "t3.micro"
-tags = {
-    name = "ec2-instance"
+  ami           = "ami-00e801948462f718a"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "ec2-instance"
+  }
 }
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/24"
+}
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/26"
+}
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.67/26"
 }
