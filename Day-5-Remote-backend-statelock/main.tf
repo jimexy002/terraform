bfa1d0f9458675344cbd7cf93514bd0a2d5b1145
Deepak <dsahu353490@gmail.com>
2026-06-05 22:23:18 +0530

    updated

--- Day-5-Remote-backend-statelock/main.tf
+++ Day-5-Remote-backend-statelock/main.tf
@@ -2,3 +2,9 @@ resource "aws_instance" "ok" {
   ami           = "ami-00e801948462f718a"
   instance_type = "t3.micro"
 }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/24"
+}
+resource "aws_vpc" "mai" {
+  cidr_block = "10.0.0.0/26"
+}
